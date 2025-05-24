1a36837ceb44ebfd1e3708eec02230b96316f518
ViktorUw <vasilishin.vitja@gmail.com>
2025-05-24 12:26:11 +0200

    100% last time

--- app/tests/test_read_docs.py
+++ app/tests/test_read_docs.py
@@ -4,9 +4,7 @@ from src.file_processor import FileProcessor
 
 def test_read_docx():
     file_processor = FileProcessor()
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../data/input"))
-    file_path = os.path.join(base_dir, "fanatyk.docx")
-    with open(file_path, "rb") as file:
+    with open("/app/data/input/fanatyk.docx", "rb") as file:
         file_processor.file = file
         file_processor.file_type = ".txt"
         content = file_processor._read_docx
--- app/tests/test_read_pdf.py
+++ app/tests/test_read_pdf.py
@@ -5,9 +5,7 @@ from src.file_processor import FileProcessor
 
 def test_read_pdf():
     file_processor = FileProcessor()
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../data/input"))
-    file_path = os.path.join(base_dir, "fanatyk.pdf")
-    with open(file_path, "rb") as file:
+    with open("/app/data/input/fanatyk.pdf", "rb") as file:
         file_processor.file = file
         file_processor.file_type = ".pdf"
         content = file_processor._read_pdf()
