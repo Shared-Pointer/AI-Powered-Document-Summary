d004acf7af67a98624dbcd9169b0b875c3c5863b
ViktorUw <vasilishin.vitja@gmail.com>
2025-05-24 12:18:20 +0200

    Fix tests 2

--- app/tests/test_read_docs.py
+++ app/tests/test_read_docs.py
@@ -3,7 +3,7 @@ from src.file_processor import FileProcessor
 
 def test_read_docx():
     file_processor = FileProcessor()
-    with open("data/input/fanatyk.docx", "rb") as file:
+    with open("../../data/input/fanatyk.docx", "rb") as file:
         file_processor.file = file
         file_processor.file_type = ".txt"
         content = file_processor._read_docx
--- app/tests/test_read_pdf.py
+++ app/tests/test_read_pdf.py
@@ -4,7 +4,7 @@ from src.file_processor import FileProcessor
 
 def test_read_pdf():
     file_processor = FileProcessor()
-    with open("data/input/fanatyk.pdf", "rb") as file:
+    with open("../../data/input/fanatyk.pdf", "rb") as file:
         file_processor.file = file
         file_processor.file_type = ".pdf"
         content = file_processor._read_pdf()
