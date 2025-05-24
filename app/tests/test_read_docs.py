2d59a699d511e1cc5f93d6a9e95300c7fed19e41
ViktorUw <vasilishin.vitja@gmail.com>
2025-05-24 11:59:38 +0200

    Add read pdf, docs tests

---
+++ app/tests/test_read_docs.py
@@ -0,0 +1,10 @@
+import pytest
+from src.file_processor import FileProcessor
+
+def test_read_docx():
+    file_processor = FileProcessor()
+    with open("data/input/test.docx", "rb") as file:
+        file_processor.file = file
+        file_processor.file_type = ".txt"
+        content = file_processor._read_docx
+        assert len(content) > 0, "Your summarize is empty"
---
+++ app/tests/test_read_pdf.py
@@ -0,0 +1,12 @@
+import pytest
+from src.file_processor import FileProcessor
+
+
+def test_read_pdf():
+    file_processor = FileProcessor()
+    with open("data/input/fanatyk.pdf", "rb") as file:
+        file_processor.file = file
+        file_processor.file_type = ".pdf"
+        content = file_processor._read_pdf()
+        assert len(content) > 0, "Your summarize is empty"
+    
